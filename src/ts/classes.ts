0b764004d981a5546d3d35db681f950039295d6e
yourfriend <friend@yourfriend.lv>
2022-09-22 23:01:10 +0300

    todos + formatting

--- src/ts/classes.ts
+++ src/ts/classes.ts
@@ -1,267 +1,304 @@
 interface Tileinfo {
-	width: number
-	height: number
+  width: number;
+  height: number;
 }
 
 interface EntitySettings {
-	ticking: {
-		function: Function;
-		speed: number;
-	}
+  ticking: {
+    function: Function;
+    speed: number;
+  };
 }
 
 interface GameSettings {
-	ground: {
-		main: string,
-		second: string
-	},
-	flowers: {
-		flowers: string[],
-		mushrooms: string[],
-		unique: string[]
-	},
-	pixelSize: number
+  ground: {
+    main: string;
+    second: string;
+  };
+  flowers: {
+    flowers: string[];
+    mushrooms: string[];
+    unique: string[];
+  };
+  pixelSize: number;
 }
 
 export class Tilemap {
-	tileSettings: Tileinfo = {width: 8, height: 8};
-	file: string;
+  tileSettings: Tileinfo = { width: 8, height: 8 };
+  file: string;
 
-	colorMap: Record<string, HTMLImageElement> = {};
-	
-	colorToTile(color: string): HTMLImageElement|undefined {
-		if(this.colorMap?.[color]) return this.colorMap[color];
-		return;
-	}
+  colorMap: Record<string, HTMLImageElement> = {};
 
-	constructor(file: string, tileSettings: Tileinfo = {width: 8, height: 8}) {
-		this.tileSettings = tileSettings
-		this.file = file;
+  colorToTile(color: string): HTMLImageElement | undefined {
+    if (this.colorMap?.[color]) return this.colorMap[color];
+    return;
+  }
 
-		const canvas = document.createElement("canvas");
-		const ctx = canvas.getContext("2d")!;
-		const img = new Image();
+  constructor(file: string, tileSettings: Tileinfo = { width: 8, height: 8 }) {
+    this.tileSettings = tileSettings;
+    this.file = file;
 
-		img.addEventListener('load', () => {
-			canvas.width = img.width;
-			canvas.height = img.height;
-			const rows = canvas.width / this.tileSettings.width;
-			const cols = canvas.height / this.tileSettings.height;
-			let num = 0;
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d")!;
+    const img = new Image();
 
-			ctx.drawImage(img, 0, 0);
-			
-			for(let c = 0; c < cols; c++) {
-				for(let r = 0; r < rows; r++) {
-					num++;
+    img.addEventListener("load", () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const rows = canvas.width / this.tileSettings.width;
+      const cols = canvas.height / this.tileSettings.height;
+      let num = 0;
 
-					let imageDataCANVAS = document.createElement('canvas');
-					let imageDataCTX = imageDataCANVAS.getContext('2d')!;
-					imageDataCANVAS.width = this.tileSettings.width;
-					imageDataCANVAS.height = this.tileSettings.height;
-					imageDataCTX.putImageData(ctx.getImageData(r * this.tileSettings.width, 
-						c * this.tileSettings.height, 
-						this.tileSettings.width, 
-						this.tileSettings.height), 0, 0);
+      ctx.drawImage(img, 0, 0);
 
-					let imageDataImg = new Image();
-					imageDataImg.src = imageDataCANVAS.toDataURL();
-					
-					this.colorMap[num.toString().padStart(6, "0")] = imageDataImg;
-				}	
-			}
-		}, false);
+      for (let c = 0; c < cols; c++) {
+        for (let r = 0; r < rows; r++) {
+          num++;
 
-		img.src = this.file; 
-	}
+          let imageDataCANVAS = document.createElement("canvas"); // THESE VARIABLE NAMES SUCK
+          let imageDataCTX = imageDataCANVAS.getContext("2d")!;
+          imageDataCANVAS.width = this.tileSettings.width;
+          imageDataCANVAS.height = this.tileSettings.height;
+          imageDataCTX.putImageData(
+            ctx.getImageData(
+              r * this.tileSettings.width,
+              c * this.tileSettings.height,
+              this.tileSettings.width,
+              this.tileSettings.height,
+            ),
+            0,
+            0,
+          );
+
+          let imageDataImg = new Image();
+          imageDataImg.src = imageDataCANVAS.toDataURL();
+
+          this.colorMap[num.toString().padStart(6, "0")] = imageDataImg;
+        }
+      }
+    }, false);
+
+    img.src = this.file;
+  }
 }
 
 export class Game {
-	canvas: HTMLCanvasElement;
-	ctx: CanvasRenderingContext2D;
-	mapLayer: Map<string, string[]> = new Map<string, string[]>();
-	entities: Map<string, Entity> = new Map<string, Entity>();
-	tileMap: Tilemap | undefined;
-	settings: GameSettings
-	debug = localStorage.getItem("isDebug?") || false;
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+  mapLayer: Map<string, string[]> = new Map<string, string[]>();
+  entities: Map<string, Entity> = new Map<string, Entity>();
+  tileMap: Tilemap | undefined;
+  settings: GameSettings;
+  debug = localStorage.getItem("isDebug?") || false;
 
-	colorAlgo(x: number, y: number) {
-		let colors: string[] = [((x+y)%2==0) ? ((Math.floor(Math.random() * 20) < 10) ? this.settings.ground.second :  this.settings.ground.main) : this.settings.ground.main];
-		
-		this.settings.flowers.flowers.forEach(x => {
-			if(Math.random() >= 0.95) {
-				colors.push(x)
-			}
-		})
-		
-		if(colors.length >= 2) return colors; // kinda ugly
-		this.settings.flowers.mushrooms.forEach(x => {
-			if(Math.random() >= 0.96) {
-				colors.push(x)
-			}
-		})
+  colorAlgo(x: number, y: number) {
+    let colors: string[] = [
+      ((x + y) % 2 == 0)
+        ? ((Math.floor(Math.random() * 20) < 10)
+          ? this.settings.ground.second
+          : this.settings.ground.main)
+        : this.settings.ground.main,
+    ]; // VERY UGLY
 
-		if(colors.length >= 2) return colors;
-		this.settings.flowers.unique.forEach(x => {
-			if(Math.random() >= 0.99) {
-				colors.push(x)
-			}
-		})
+    this.settings.flowers.flowers.forEach((x) => {
+      if (Math.random() >= 0.95) {
+        colors.push(x);
+      }
+    });
 
-		return colors;
-	}
+    if (colors.length >= 2) return colors; // kinda ugly
+    this.settings.flowers.mushrooms.forEach((x) => {
+      if (Math.random() >= 0.96) {
+        colors.push(x);
+      }
+    });
 
-	coordinatesToPixel(x: number): number {
-		return this.settings.pixelSize * Math.floor(x);
-	} 
-	
-	constructor(canvas: HTMLCanvasElement, settings: GameSettings = {
-		ground: {
-			main: "000146",
-			second: "000145"
-		},
-		flowers: {
-			flowers: ["000062", "000061", "000060"],
-			mushrooms: ["000032"],
-			unique: ["000038"]
-		},
-		pixelSize: 30
-	}) {
-		this.canvas = canvas;
-		this.ctx = this.canvas.getContext("2d")!;
-		this.canvas.width = innerWidth;
-		this.canvas.height = innerHeight;
-		this.settings = settings;
+    if (colors.length >= 2) return colors;
+    this.settings.flowers.unique.forEach((x) => {
+      if (Math.random() >= 0.99) {
+        colors.push(x);
+      }
+    });
 
-		window.requestAnimationFrame(() => {this.draw()});
+    return colors;
+  }
 
-		document.body.addEventListener("keydown", (e) => {
-			if(e.repeat) return;
-			this.entities.forEach(x => x.moveHandlers.forEach(y => y(e.key, true)))
-		});
+  coordinatesToPixel(x: number): number {
+    return this.settings.pixelSize * Math.floor(x);
+  }
 
-		document.body.addEventListener("keyup", (e) => {
-			this.entities.forEach(x => x.moveHandlers.forEach(y => y(e.key, false)))
-		});
+  constructor(canvas: HTMLCanvasElement, settings: GameSettings = {
+    ground: {
+      main: "000146",
+      second: "000145",
+    },
+    flowers: {
+      flowers: ["000062", "000061", "000060"],
+      mushrooms: ["000032"],
+      unique: ["000038"],
+    },
+    pixelSize: 30,
+  }) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d")!;
+    this.canvas.width = innerWidth;
+    this.canvas.height = innerHeight;
+    this.settings = settings;
 
+    window.requestAnimationFrame(() => {
+      this.draw();
+    });
 
-		for(let x = 0; x < canvas.width; x+=this.settings.pixelSize) {
-			for(let y = 0; y < canvas.height; y+=this.settings.pixelSize) {				
-				this.mapLayer.set(x+","+y, this.colorAlgo(x, y));
-			}
-		}
+    document.body.addEventListener("keydown", (e) => {
+      if (e.repeat) return;
+      this.entities.forEach((x) =>
+        x.moveHandlers.forEach((y) => y(e.key, true))
+      );
+    });
 
-	}
+    document.body.addEventListener("keyup", (e) => {
+      this.entities.forEach((x) =>
+        x.moveHandlers.forEach((y) => y(e.key, false))
+      );
+    });
 
-	draw() {
-		this.canvas.width = innerWidth;
-		this.canvas.height = innerHeight;
-		let total = 0;
-		
+    for (let x = 0; x < canvas.width; x += this.settings.pixelSize) {
+      for (let y = 0; y < canvas.height; y += this.settings.pixelSize) {
+        this.mapLayer.set(x + "," + y, this.colorAlgo(x, y));
+      }
+    }
+  }
 
-		for(let x = 0; x < this.canvas.width; x+=this.settings.pixelSize) {
-			for(let y = 0; y < this.canvas.height; y+=this.settings.pixelSize) {
-				total++;
-				if(!this.tileMap) {
-					this.ctx.fillStyle = "#" + this.mapLayer.get(x+","+y);
-					this.ctx.fillRect(x, y, this.settings.pixelSize, this.settings.pixelSize);
-					continue;
-				}
+  draw() {
+    this.canvas.width = innerWidth;
+    this.canvas.height = innerHeight;
+    let total = 0;
 
-				let colors: [Tilemap, string[]][] = [];
+    for (let x = 0; x < this.canvas.width; x += this.settings.pixelSize) {
+      for (let y = 0; y < this.canvas.height; y += this.settings.pixelSize) {
+        total++;
+        if (!this.tileMap) {
+          this.ctx.fillStyle = "#" + this.mapLayer.get(x + "," + y);
+          this.ctx.fillRect(
+            x,
+            y,
+            this.settings.pixelSize,
+            this.settings.pixelSize,
+          );
+          continue;
+        }
 
-				if(!this.mapLayer.get(x+","+y)) {
-					this.mapLayer.set(x+","+y, this.colorAlgo(x, y));
-					continue;
-				}
-				
-				colors.push([this.tileMap, this.mapLayer.get(x+","+y)!]);
+        let colors: [Tilemap, string[]][] = [];
 
-				colors.forEach((element)=> {
-					element[1].forEach(currcolor => {
-						let tile = element[0].colorToTile(currcolor);
-						
-						if(this.debug) {
-							tile = element[0].colorToTile(total.toString().padStart(6, "0"))
-						}
+        if (!this.mapLayer.get(x + "," + y)) {
+          this.mapLayer.set(x + "," + y, this.colorAlgo(x, y));
+          continue;
+        }
 
-						if(tile) {
-							this.ctx.drawImage(tile, x, y);
-							
-							if(this.debug) {
-								this.ctx.fillStyle = "#ffffff"
-								this.ctx.font = "15px Arial"
+        colors.push([this.tileMap, this.mapLayer.get(x + "," + y)!]);
 
-								this.ctx.fillText(total.toString(), x+2, y+15);
-							}
-						}
+        colors.forEach((element) => {
+          element[1].forEach((currcolor) => {
+            let tile = element[0].colorToTile(currcolor);
 
-					})
-				})
-			}
-		}
+            if (this.debug) {
+              tile = element[0].colorToTile(total.toString().padStart(6, "0"));
+            }
 
-		this.entities.forEach((e, i) => {
-			if(e.tileMap) {
-				const xd = e.tileMap.colorToTile(e.color) ;
-				if(xd) {
-					this.ctx.drawImage(xd, e.cX, e.cY);
-				}
-			} else {
-				this.ctx.fillStyle = "#" + e.color;
-				this.ctx.fillRect(e.cX, e.cY, this.settings.pixelSize, this.settings.pixelSize);
-				return;
-			}
-		})
+            if (tile) {
+              this.ctx.drawImage(tile, x, y);
 
-		window.requestAnimationFrame(() => {this.draw()})
-	}
+              if (this.debug) {
+                this.ctx.fillStyle = "#ffffff";
+                this.ctx.font = "15px Arial";
+
+                this.ctx.fillText(total.toString(), x + 2, y + 15);
+              }
+            }
+          });
+        });
+      }
+    }
+
+    this.entities.forEach((e, i) => {
+      if (e.tileMap) {
+        const xd = e.tileMap.colorToTile(e.color);
+        if (xd) {
+          this.ctx.drawImage(xd, e.cX, e.cY);
+        }
+      } else {
+        this.ctx.fillStyle = "#" + e.color;
+        this.ctx.fillRect(
+          e.cX,
+          e.cY,
+          this.settings.pixelSize,
+          this.settings.pixelSize,
+        );
+        return;
+      }
+    });
+
+    window.requestAnimationFrame(() => {
+      this.draw();
+    });
+  }
 }
 export class Entity {
-	private game: Game;
-    private readonly settings: EntitySettings | undefined;
-	color: string;
-    cX: number;
-    cY: number;
-	name: string;
-	tileMap: Tilemap | undefined;
-	moveHandlers: Function[] = [];
+  private game: Game;
+  private readonly settings: EntitySettings | undefined;
+  color: string;
+  cX: number;
+  cY: number;
+  name: string;
+  tileMap: Tilemap | undefined;
+  moveHandlers: Function[] = [];
 
-	constructor(game: Game, name: string, color: string, x: number, y: number, settings: EntitySettings|undefined = undefined) {
-		if(game.debug) return; // no entities in debug mode
-		
-		console.log("✨ new entity created:", name, this)
-		this.game = game;
-		this.color = color;
-		this.settings = settings;
-		this.name = name;
-		
-		this.moveTo(x,y);
+  constructor(
+    game: Game,
+    name: string,
+    color: string,
+    x: number,
+    y: number,
+    settings: EntitySettings | undefined = undefined,
+  ) {
+    if (game.debug) return; // no entities in debug mode
 
-		if(this.settings?.ticking?.function && this.settings?.ticking?.speed) {
-			setInterval(() => {
-				this.settings?.ticking.function(this);
-			}, this.settings.ticking.speed);
-		}
+    console.log("✨ new entity created:", name, this);
+    this.game = game;
+    this.color = color;
+    this.settings = settings;
+    this.name = name;
 
-		this.game.entities.set(this.name, this);
-	}
-	
-	addMoveHandler(func: Function) {
-		this.moveHandlers.push(func);
-	}
+    this.moveTo(x, y);
 
-	moveTo(x: number, y: number) {
-		if([...this.game.entities.values()].find(b => 
-			b.cX == x && 
-			b.cY == y)
-		) return;
+    if (this.settings?.ticking?.function && this.settings?.ticking?.speed) {
+      setInterval(() => {
+        this.settings?.ticking.function(this);
+      }, this.settings.ticking.speed);
+    }
 
-		if((x > this.game.canvas.width || x < 0) ||
-			(y > this.game.canvas.height || y < 0)) {
-				return;
-			};
-		this.cX = x;
-		this.cY = y;
-	}
+    this.game.entities.set(this.name, this);
+  }
+
+  addMoveHandler(func: Function) {
+    this.moveHandlers.push(func);
+  }
+
+  moveTo(x: number, y: number) {
+    if (
+      [...this.game.entities.values()].find((b) =>
+        b.cX == x &&
+        b.cY == y
+      )
+    ) return;
+
+    if (
+      (x > this.game.canvas.width || x < 0) ||
+      (y > this.game.canvas.height || y < 0)
+    ) {
+      return;
+    }
+    this.cX = x;
+    this.cY = y;
+  }
 }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,61 +5,59 @@ let moving: Record<string, boolean> = {};
 const game = new Game(document.getElementById("canvas") as HTMLCanvasElement);
 
 const player = new Entity(game, "player1", "000001", 200, 200, {
-    ticking: {
-        function: () => {
-            if(moving["w"]) {
-                player.moveTo(player.cX, player.cY - 2);
-                player.color = "000003";
-                return;
+  ticking: {
+    function: () => {
+      if (moving["w"]) {
+        player.moveTo(player.cX, player.cY - 2);
+        player.color = "000003";
+        return;
+      }
+      if (moving["s"]) {
+        player.moveTo(player.cX, player.cY + 2);
+        player.color = "000001";
+        return;
+      }
 
-            }
-            if(moving["s"]) {
-                player.moveTo(player.cX, player.cY + 2);
-                player.color = "000001"
-                return;
-            }
+      if (moving["a"]) {
+        player.moveTo(player.cX - 2, player.cY);
+        player.color = "000002";
+        return;
+      }
 
-            if(moving["a"]) {
-                player.moveTo(player.cX - 2, player.cY);
-                player.color = "000002"
-                return;
-            }
-
-            if(moving["d"]) {
-                player.moveTo(player.cX + 1, player.cY);
-                player.color = "000004"
-                return;
-            }
-        },
-        speed: 10
-    }
+      if (moving["d"]) {
+        player.moveTo(player.cX + 1, player.cY);
+        player.color = "000004";
+        return;
+      }
+    },
+    speed: 10,
+  },
 });
 
 game.tileMap = new Tilemap("dark_forest.png", {
-    width: 32,
-    height: 32
+  width: 32,
+  height: 32,
 });
 
 player.tileMap = new Tilemap("playersprite.png", {
-    width: 32,
-    height: 32
+  width: 32,
+  height: 32,
 });
 
-
-window.addEventListener("keydown", e => {    
-    switch (e.key) {
-        case 'ContextMenu':
-            if(e.preventDefault) e.preventDefault();
-            if(localStorage.getItem("isDebug?")) {
-                localStorage.removeItem("isDebug?")
-            } else {
-                localStorage.setItem("isDebug?", "true");
-            }
-            window.location.reload();
-        break;
-    }
-})
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ContextMenu":
+      if (e.preventDefault) e.preventDefault();
+      if (localStorage.getItem("isDebug?")) {
+        localStorage.removeItem("isDebug?");
+      } else {
+        localStorage.setItem("isDebug?", "true");
+      }
+      window.location.reload();
+      break;
+  }
+});
 
 player.addMoveHandler((key, x) => {
-    moving[key] = x;
+  moving[key] = x;
 });
